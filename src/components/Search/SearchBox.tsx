47256c7ee941a7d25b8612245727af4b6c800cc3
Henrik Svendsgård <hes@itumx.no>
2024-08-28 11:13:36 +0200

    fixed searchbox styling

--- src/components/Search/SearchBox.tsx
+++ src/components/Search/SearchBox.tsx
@@ -3,7 +3,7 @@ import SearchBar from "./SearchBar";
 export default function SearchBox({ inHeader }: { inHeader?: boolean }) {
 	return (
 		<>
-			<div className={`mx-auto w-full px-10 ${inHeader && "w-96 px-0"}`}>
+			<div className={`${!inHeader && "mx-auto w-full px-10"} ${inHeader && "w-[300px] px-0"}`}>
 				<SearchBar />
 			</div>
 		</>
