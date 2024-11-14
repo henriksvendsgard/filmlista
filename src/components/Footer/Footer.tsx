1559f6211c824bec2bcfffcd4fba2f6936bdd20e
Henrik Svendsgård <hes@itumx.no>
2024-11-14 15:16:16 +0100

    style: footer text color on lightmode

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Footer() {
 	return (
 		<div className="flex mt-auto mb-0 pt-10">
-			<div className="w-full bg-background border-t text-white px-5 py-8 flex justify-between flex-wrap gap-4">
+			<div className="w-full bg-background border-t px-5 py-8 flex justify-between flex-wrap gap-4">
 				<p>&copy; 2024 Filmlista</p>
 				<div className="flex items-center gap-1">
 					<p>Utviklet av</p>
